1584468b554f85ba51829a7e01ea0bdcdac164fb
crouse <quqinglei@icloud.com>
2015-04-14 17:51:47 +0800

    ok

--- scheduler/sched.c
+++ scheduler/sched.c
@@ -16,7 +16,9 @@
 #define ERROR_CONNECT_MYSQL 2
 #define ERROR_QUERY_MYSQL 3
 
-int create_dbs();
+void help();
+int init_db_connections();
+int create_tables();
 
 char *appname = NULL;
 typedef struct {
@@ -29,7 +31,75 @@ typedef struct {
 
 gconf_t gconf;
 
-int init_db() // init database connections
+/*[s] get_opt */
+static char* const short_options = "h:u:p:n:i";
+
+struct option long_options[] = {
+    {"hostname", 1, NULL, 'h'},
+    {"username", 1, NULL, 'u'},
+    {"password", 1, NULL, 'p'},
+    {"dbname", 1, NULL, 'n'},
+    {"init", 0, NULL, 'i'},
+    {0, 0, 0, 0},
+};
+/*[e] get opt */
+
+int main(int argc, char *argv[])
+{
+    /*[s] set default database name */
+    char* default_dbname = "prajna";
+    memset(gconf.dbname, '\0', COMMON_NAME_LEN);
+    memcpy(gconf.dbname, default_dbname, COMMON_NAME_LEN);
+    /*[e] set default database name */
+
+    appname = argv[0];
+    if (argc < 7) { help(); }
+
+    int c;
+    int initdb = FALSE; // 1: init database if is not created
+    while((c = getopt_long(argc, argv, short_options, long_options, NULL)) != -1)
+    {
+        switch (c) {
+            case 'h':
+                memset(gconf.hostname, '\0', HOST_LEN);
+                memcpy(gconf.hostname, optarg, strlen(optarg));
+                break;
+            case 'u': 
+                memset(gconf.username, '\0', COMMON_NAME_LEN);
+                memcpy(gconf.username, optarg, strlen(optarg));
+                break;
+            case 'p':
+                memset(gconf.password, '\0', PASS_LEN);
+                memcpy(gconf.password, optarg, strlen(optarg));
+                break;
+            case 'i':
+                initdb = TRUE;
+                break;
+            case 'n':
+                memset(gconf.dbname, '\0', COMMON_NAME_LEN);
+                memcpy(gconf.dbname, optarg, strlen(optarg));
+                break;
+            default:
+                help();
+                break;
+        }
+    }
+
+    init_db_connections();
+    if (initdb == TRUE) create_tables();
+    return 0;
+}
+
+void help()
+{
+    printf("Usage: %s --hostname xxx --dbname prajna --username xxx --password xxx\n", appname);
+    printf("\t\"hostname: mysql hostname\"\n");
+    printf("\t\"username: mysql username\"\n");
+    printf("\t\"password: mysql password\"\n");
+    exit(-1);
+}
+
+int init_db_connections() // init database connections
 {
    int i;
    for(i = 0; i < CON_NUM; i++) {
@@ -40,7 +110,14 @@ int init_db() // init database connections
        }
 
        if (mysql_real_connect(gconf.con[i], gconf.hostname, gconf.username, \
-                   gconf.password, NULL, 0, NULL, 0) == NULL) {
+                   gconf.password, gconf.dbname, 0, NULL, 0) == NULL) {
+           fprintf(stderr, "%s\n", mysql_error(gconf.con[i]));
+           mysql_close(gconf.con[i]);
+           return ERROR_CONNECT_MYSQL;
+       }
+
+
+       if (mysql_query(gconf.con[i], "set character set utf8")) {
            fprintf(stderr, "%s\n", mysql_error(gconf.con[i]));
            mysql_close(gconf.con[i]);
            return ERROR_CONNECT_MYSQL;
@@ -50,95 +127,102 @@ int init_db() // init database connections
    return 0;
 }
 
-/*[s] get_opt */
-static char* const short_options = "h:u:p:i";
-
-struct option long_options[] = {
-    {"hostname", 1, NULL, 'h'},
-    {"username", 1, NULL, 'u'},
-    {"password", 1, NULL, 'p'},
-    {"init", 0, NULL, 'i'},
-    {0, 0, 0, 0},
-};
-/*[e] get opt */
-
-
-void help()
+int create_tables()
 {
-    printf("Usage: %s --hostname xxx --username xxx --password xxx\n", appname);
-    exit(-1);
-}
+    char *t1 = "CREATE TABLE IF NOT EXISTS `sched_manage` (\
+                `sched_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '任务唯一ID，自增',\
+                `depends` varchar(255) DEFAULT NULL COMMENT '任务依赖关系',\
+                `hostname` varchar(31) NOT NULL DEFAULT 'localhost' COMMENT '主机名',\
+                `user_os` varchar(31) NOT NULL DEFAULT 'root' COMMENT '操作系统用户',\
+                `env` varchar(255) DEFAULT NULL COMMENT '环境变量，默认为空，则使用系统环境变量',\
+                `sched_type` tinyint(4) DEFAULT '0' COMMENT '0: bash, 1: hadoop, 2: spark, 3:other',\
+                `crontab` varchar(127) NOT NULL COMMENT 'crontab 表达式',\
+                `priority` tinyint(3) unsigned DEFAULT '0' COMMENT '执行优先级，越小优先级越大',\
+                `cmdline` varchar(255) DEFAULT NULL COMMENT 'bash 任务，命令行',\
+                `appname` varchar(63) DEFAULT NULL COMMENT '程序名称，或者绝对路径',\
+                `appparas` varchar(511) DEFAULT NULL COMMENT '程序参数列表',\
+                `dbname` varchar(63) DEFAULT NULL COMMENT '如果执行存过，则提供数据库名称, 其他情况为空',\
+                `timeout` int(3) unsigned NOT NULL DEFAULT '30' COMMENT '程序执行超时时间，默认为30分钟',\
+                `cstatus` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '0: 新添加未调度, 1: 已经调度, 2: 修改, 3: 需要删除',\
+                `desc` varchar(255) DEFAULT NULL COMMENT '任务描述',\
+                `dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',\
+                PRIMARY KEY (`sched_id`)\
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;\
+                ";
 
-int main(int argc, char *argv[])
-{
-    appname = argv[0];
-    if (argc != 7) { help(); }
+    if (mysql_query(gconf.con[0], t1)) goto MY_ERROR; // create table sched_manage
 
-    int c;
-    int initdb = 0; // 1: init database if is not created
-    while((c = getopt_long(argc, argv, short_options, long_options, NULL)) != -1)
-    {
-        switch (c) {
-            case 'h':
-                memcpy(gconf.hostname, optarg, strlen(optarg));
-                break;
-            case 'u': 
-                memcpy(gconf.dbname, optarg, strlen(optarg));
-                break;
-            case 'p':
-                memcpy(gconf.password, optarg, strlen(optarg));
-                break;
-            case 'i':
-                initdb = 1;
-            default:
-                help();
-                break;
-        }
-    }
+    char *t2 = "CREATE TABLE IF NOT EXISTS `sched_formal` (\
+                `sched_id` int(11) NOT NULL COMMENT '任务唯一ID，取自表: sched_manage: sched_id',\
+                `depends` varchar(255) DEFAULT NULL COMMENT '任务依赖关系',\
+                `hostname` varchar(31) NOT NULL DEFAULT 'localhost' COMMENT '主机名',\
+                `user_os` varchar(31) NOT NULL DEFAULT 'root' COMMENT '操作系统用户',\
+                `env` varchar(255) DEFAULT NULL COMMENT '环境变量，默认为空，则使用系统环境变量',\
+                `sched_type` tinyint(4) DEFAULT '0' COMMENT '0: bash, 1: hadoop, 2: spark, 3:other',\
+                `crontab` varchar(127) NOT NULL COMMENT 'crontab 表达式',\
+                `priority` tinyint(3) unsigned DEFAULT '0' COMMENT '执行优先级，越小优先级越大',\
+                `cmdline` varchar(255) DEFAULT NULL COMMENT 'bash 任务，命令行',\
+                `appname` varchar(63) DEFAULT NULL COMMENT '程序名称，或者绝对路径',\
+                `appparas` varchar(511) DEFAULT NULL COMMENT '程序参数列表',\
+                `dbname` varchar(63) DEFAULT NULL COMMENT '如果执行存过，则提供数据库名称, 其他情况为空',\
+                `timeout` int(3) unsigned NOT NULL DEFAULT '30' COMMENT '程序执行超时时间，默认为30分钟',\
+                `desc` varchar(255) DEFAULT NULL COMMENT '任务描述',\
+                `dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',\
+                PRIMARY KEY (`sched_id`)\
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;\
+                ";
 
-    init_db();
-    create_dbs();
+    if (mysql_query(gconf.con[0], t2)) goto MY_ERROR; // create table sched_manage
+
+    char *t3 = "CREATE TABLE IF NOT EXISTS `sched_status` (\
+                `sched_id` int(11) NOT NULL COMMENT '任务唯一ID，取自表: sched_manage: sched_id',\
+                `depends` varchar(255) DEFAULT NULL COMMENT '任务依赖关系',\
+                `hostname` varchar(31) NOT NULL DEFAULT 'localhost' COMMENT '主机名',\
+                `user_os` varchar(31) NOT NULL DEFAULT 'root' COMMENT '操作系统用户',\
+                `env` varchar(255) DEFAULT NULL COMMENT '环境变量，默认为空，则使用系统环境变量',\
+                `sched_type` tinyint(4) DEFAULT '0' COMMENT '0: bash, 1: hadoop, 2: spark, 3:other',\
+                `crontab` varchar(127) NOT NULL COMMENT 'crontab 表达式',\
+                `priority` tinyint(3) unsigned DEFAULT '0' COMMENT '执行优先级，越小优先级越大',\
+                `cmdline` varchar(255) DEFAULT NULL COMMENT 'bash 任务，命令行',\
+                `appname` varchar(63) DEFAULT NULL COMMENT '程序名称，或者绝对路径',\
+                `appparas` varchar(511) DEFAULT NULL COMMENT '程序参数列表',\
+                `dbname` varchar(63) DEFAULT NULL COMMENT '如果执行存过，则提供数据库名称, 其他情况为空',\
+                `timeout` int(3) unsigned NOT NULL DEFAULT '30' COMMENT '程序执行超时时间，默认为30分钟',\
+                `runstatus` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '运行状态，0：未运行，1：运行中 2：超时 3：错误',\
+                `desc` varchar(255) DEFAULT NULL COMMENT '任务描述',\
+                `dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',\
+                PRIMARY KEY (`sched_id`)\
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;\
+                ";
+
+    if (mysql_query(gconf.con[0], t3)) goto MY_ERROR; // create table sched_manage
+
+    char *t4 = "CREATE TABLE IF NOT EXISTS `sched_history` (\
+                `sched_id` int(11) NOT NULL COMMENT '任务唯一ID，取自表: sched_manage: sched_id',\
+                `depends` varchar(255) DEFAULT NULL COMMENT '任务依赖关系',\
+                `hostname` varchar(31) NOT NULL DEFAULT 'localhost' COMMENT '主机名',\
+                `user_os` varchar(31) NOT NULL DEFAULT 'root' COMMENT '操作系统用户',\
+                `env` varchar(255) DEFAULT NULL COMMENT '环境变量，默认为空，则使用系统环境变量',\
+                `sched_type` tinyint(4) DEFAULT '0' COMMENT '0: bash, 1: hadoop, 2: spark, 3:other',\
+                `crontab` varchar(127) NOT NULL COMMENT 'crontab 表达式',\
+                `priority` tinyint(3) unsigned DEFAULT '0' COMMENT '执行优先级，越小优先级越大',\
+                `cmdline` varchar(255) DEFAULT NULL COMMENT 'bash 任务，命令行',\
+                `appname` varchar(63) DEFAULT NULL COMMENT '程序名称，或者绝对路径',\
+                `appparas` varchar(511) DEFAULT NULL COMMENT '程序参数列表',\
+                `dbname` varchar(63) DEFAULT NULL COMMENT '如果执行存过，则提供数据库名称, 其他情况为空',\
+                `timeout` int(3) unsigned NOT NULL DEFAULT '30' COMMENT '程序执行超时时间，默认为30分钟',\
+                `desc` varchar(255) DEFAULT NULL COMMENT '任务描述',\
+                `dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',\
+                PRIMARY KEY (`sched_id`)\
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;\
+                ";
+
+    if (mysql_query(gconf.con[0], t4)) goto MY_ERROR; // create table sched_manage
+
     return 0;
+
+MY_ERROR:
+    fprintf(stderr, "[%d] %s\n", ERROR_QUERY_MYSQL, mysql_error(gconf.con[0]));
+    mysql_close(gconf.con[0]);
+    return ERROR_QUERY_MYSQL;
 }
-
-int create_dbs()
-{
-    char *dbsql = "create database prajna;";
-    if (mysql_query(gconf.con[0], dbsql)) {
-        fprintf(stderr, "%s\n", mysql_error(gconf.con[0]));
-        mysql_close(gconf.con[0]);
-        return ERROR_QUERY_MYSQL;
-    }
-
-    char *sql = "\
-                 CREATE TABLE `sched_modify`
-        ";
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
